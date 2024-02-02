e14bf8794d6e6feefd799295a6df8b6dedd6e744
Daryna Hnidash <melissa998877@gmail.com>
2024-02-02 16:31:55 +0200

    improved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+let slideIndex = 1;
+
+const buttonLeft = document.querySelector('#button-left');
+const buttonRight = document.querySelector('#button-right');
+
+buttonLeft.addEventListener('click', () => {
+  slideIndex--
+  showDivs(slideIndex);
+});
+
+buttonRight.addEventListener('click', () => {
+  slideIndex++
+  showDivs(slideIndex);
+});
+
+function showDivs(n) {
+  const slides = document.querySelectorAll('.carousel__slide');
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  };
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  };
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.add('carousel__slide--hidden');
+  }
+
+  slides[slideIndex - 1].classList.remove('carousel__slide--hidden');
+}
